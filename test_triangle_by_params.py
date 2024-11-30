8ccca9edb8a129f0697c2fac46df44e1d1e388b7
Minjune Kim <mjk32384@gmail.com>
2024-11-30 05:59:49 +0000

    triangle vs kagome analysis

--- test_triangle_by_params.py
+++ test_triangle_by_params.py
@@ -13,11 +13,11 @@ try:
 except:
     mapdl = launch_mapdl(start_instance=True, port=50052)
 
-for n_cell in range(2, 8):
-    for relative_density in np.arange(0.15, 0.55, 0.1):
-        lattice_tensile_test(mapdl, lattice_type='triangle_h', mesh_size=0.3, n_cell=n_cell, relative_density=relative_density,
-                             three_dim=False, filename=f'n{n_cell}_r{int(relative_density*100)}', external_wall=True)
-for n_cell in range(2, 8):
+# for n_cell in range(2, 8):
+#     for relative_density in np.arange(0.15, 0.55, 0.1):
+#         lattice_tensile_test(mapdl, lattice_type='triangle_h', mesh_size=0.3, n_cell=n_cell, relative_density=relative_density,
+#                              three_dim=False, filename=f'n{n_cell}_r{int(relative_density*100)}', external_wall=True)
+for n_cell in range(6, 8):
     for relative_density in np.arange(0.15, 0.55, 0.1):
         lattice_tensile_test(mapdl, lattice_type='triangle_h', mesh_size=0.3, n_cell=n_cell, relative_density=relative_density,
                              three_dim=False, filename=f'n{n_cell}_r{int(relative_density*100)}', external_wall=False)
